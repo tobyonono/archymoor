07355e7fbf377317c74aef1a8e99f8a2f85dcaf9
tobyonono <ononop@tcd.ie>
2023-04-12 23:04:30 +0100

    ..

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,8 +18,8 @@ const Footer = () => {
                 </div>
             </div>
             <div className='fixed bottom-0 left-0 right-0'>
-                <button onClick={() => setShowModal(!showModal)} className='w-full font-mono py-1 text-center text-black h-8 bg-white/100 hover:bg-white/90 uppercase'>
-                    005 - Words
+                <button onClick={() => setShowModal(!showModal)} className='w-full font-mono py-1 text-center text-black h-8 bg-white/100 hover:bg-white/90  uppercase'>
+                    <span className='hover:animate-bounce'>005 - Words</span>
                 </button>
             </div>
         </footer>
